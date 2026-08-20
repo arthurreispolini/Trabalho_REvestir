7f1a8f544f82e04bf7e00e484a8b6ac96b5fc181
arthurreispolini <arthurreispolini@gmail.com>
2026-08-19 22:02:28 -0300

    Header incrível

--- src/header.jsx
+++ src/header.jsx
@@ -8,23 +8,31 @@ import { Link } from "react-router-dom";
 
 
 function Header() {
+const[input,Setinput] = useState(false)
 
   return (
     <>
     <div className="header">
       <div className="titlediv">
       <h1 className="title">REvestir</h1>
+     <div className={`pesquisa ${input ? "aberta" : ""}`}>
+  <FiSearch
+    onClick={() => Setinput(!input)}
+    className="lupa"
+  />
+  <input
+    type="text"
+    placeholder="Pesquisar"
+    className="input_lupa"
+  />
+</div>
       </div>
       <div className="divpheader">
       <p>Início</p>
-      <p>Coleções</p>
       <p>Masculino</p>
       <p>Feminino</p>
-      <p>Acessórios</p>
       </div>
       <div className="iconescadastrocart">
-      {/* <input type="text" className="barrapesquisa" placeholder="Pesquise aqui"/> */}
-<FiSearch className="lupa"/>
 <Link to="/login">
 <FaUser  className="iconecadastro"/>
 </Link>
